e9813cd92df0e85b0a1d0d851bd1a6222576f7f9
zamanuddinkhan <zamanuddinkhan1234@gmail.com>
2026-04-30 13:07:50 +0530

    Python Fundamentals

---
+++ Lecture - 1/Var.py
@@ -0,0 +1,27 @@
+name = '''Zaman'''
+name1 = "uddin"
+name2 = 'Khan'
+print(name)
+print(name1)
+print(name2)
+print("Hello", name + name1, name2)
+
+age = 20
+price = 201.23
+
+print("My age = ", age)
+print("Price of Product = ", price)
+
+# Assign
+age1 = age
+print(age1)
+
+a = None
+old = False
+
+# Type
+print(type(name))
+print(type(price))
+print(type(age))
+print(type(a))
+print(type(old))
