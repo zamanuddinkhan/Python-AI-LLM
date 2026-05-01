d61fd044d6313dcae194e369e2436a58f4e1ade1
Zamanuddin Khan <zamanuddinkhan1234@gmail.com>
2026-05-01 16:38:09 +0530

    Refactor Day1.py to improve formatting and clarity

--- Python Libraries/NUMPY/Day1.py
+++ Python Libraries/NUMPY/Day1.py
@@ -12,7 +12,7 @@ import numpy as np
 arr1 = np.array([1,2,3,4,5,6])
 print("1D ARRAY: ",arr1,"\n")
 
-# 2D: Combination of 1D array know as 2D array.
+# 2D: Combination of 1D array known as 2D array.
 arr2 = np.array([[1,2,3],[4,5,6]])
 print("2D ARRAY: ",arr2,"\n")
 
