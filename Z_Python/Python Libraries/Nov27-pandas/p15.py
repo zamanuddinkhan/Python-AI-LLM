ce40bb69722470686ac91d02db8598af0aeedf54
Zamanuddin Khan <zamanuddinkhan1234@gmail.com>
2026-05-01 16:32:01 +0530

    Delete Z_Python/Python Libraries directory

--- Z_Python/Python Libraries/Nov27-pandas/p15.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-s = pd.Series([i for i in range(20)])
-def  odd_selection(s):
-    return [True if i % 2 == 1 else False for i in range(s.size)]
-
-print(s[odd_selection])
-print(s)
