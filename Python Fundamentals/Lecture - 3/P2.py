cc46e5e9e80c4a0661e0101fd0baf7a89833fa8a
zamanuddinkhan <zamanuddinkhan1234@gmail.com>
2026-06-06 23:16:20 +0530

    Enhance P2.py with detailed comments and examples for palindrome checking

--- Python Fundamentals/Lecture - 3/P2.py
+++ Python Fundamentals/Lecture - 3/P2.py
@@ -1,1 +1,21 @@
-# Write a Program to check if a List cotains a palindrome of elements. HINT: Use Copy() Method
+# Write a Program to check if a List cotains a Palindrome of elements. HINT: Use Copy() Method
+# A palindrome number is a number that remains the same when its digits are reversed. Ex: 121, 111, 12455421
+
+Number = [1,2,3,2,1]
+Copied_Number = Number.copy()
+Copied_Number.reverse()
+
+if Copied_Number == Number:
+    print("Palindrome Number")
+else:
+    print("Not Palindrome Number")
+
+
+# OR By using Slicing
+
+Number = [1,2,3,2,1]
+
+if Number == Number[::-1]:
+    print("\nPalindrome Number")
+else:
+    print("\nNot Palindrome Number")
