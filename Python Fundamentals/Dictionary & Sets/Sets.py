06a17fe4777fd14657be55b515c3d982761cfc47
zamanuddinkhan <zamanuddinkhan1234@gmail.com>
2026-06-07 15:44:36 +0530

    Add initial implementation of Sets.py with basic set definition and examples

---
+++ Python Fundamentals/Dictionary & Sets/Sets.py
@@ -0,0 +1,5 @@
+# Sets: It is a well definied collection of objects i.e. Unordered items. Each elements in the set must be Unique and Immutable.
+
+collection = {1,2,3,4}
+print(collection)
+print(type(collection))
