04db04cc2c47772e5b88c2f26f414f5d9a8b5f5c
zamanuddinkhan <zamanuddinkhan1234@gmail.com>
2026-06-09 00:05:02 +0530

    Add P4.py to compare two integers and print the result

---
+++ Python Fundamentals/Variables & Data Types/P4.py
@@ -0,0 +1,6 @@
+# WAP to input 2 int numbers, a and b.
+# Print True if a is greater than or equal to b. If not Print False.
+
+a = int(input("Enter A's Value: "))
+b = int(input("Enter B's Value: "))
+print(a>=b)
