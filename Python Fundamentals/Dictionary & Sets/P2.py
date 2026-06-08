3a0f3b2b74d7f1d103889beeddfbe0f1bf66a4df
zamanuddinkhan <zamanuddinkhan1234@gmail.com>
2026-06-09 02:20:09 +0530

    Add implementation to count unique subjects for classroom allocation

---
+++ Python Fundamentals/Dictionary & Sets/P2.py
@@ -0,0 +1,7 @@
+# You are given a list of subjects for students. Assume one classroom is required for 1 subject.
+# How many classroom are needed by all students.
+# "python", "java", "C++", "python", "javascript", "java", "python", "java", "C++", "C"
+
+subjects = {"python", "java", "C++", "python", "javascript", "java", "python", "java", "C++", "C"}
+print(subjects)
+print(len(subjects))
