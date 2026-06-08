c2be76512ae4493796f6e31966f67ee9f00c79c5
zamanuddinkhan <zamanuddinkhan1234@gmail.com>
2026-06-09 02:11:24 +0530

    Add initial implementation of word meanings in a Python dictionary

---
+++ Python Fundamentals/Dictionary & Sets/P1.py
@@ -0,0 +1,11 @@
+# Store Following Words Meanings in the Python Dictionary:
+# table : "a piece of furniture", "list of facts and figures"
+# cat : "a small animal"
+
+word_meanings ={
+    "table" : ["a piece of furniture", "list of facts and figures"],
+    "cat" : "a small animal"
+}
+
+print(word_meanings)
+print(type(word_meanings))
