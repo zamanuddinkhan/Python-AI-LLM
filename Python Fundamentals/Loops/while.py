953f577e4d71a0be3a4b40fff1482b0da1d76f5c
zamanuddinkhan <zamanuddinkhan1234@gmail.com>
2026-06-16 17:20:22 +0530

    Add initial implementation of while loop demonstrating specific iteration

---
+++ Python Fundamentals/Loops/while.py
@@ -0,0 +1,11 @@
+# While: Runs till condition is TRUE (When we don't know how many times we iterate)
+
+# # Infinite Times
+# while True:
+#     print("Hello World")
+
+# Specific Times
+count = 1
+while count <= 5:
+    print("Hello World")
+    count += 1
