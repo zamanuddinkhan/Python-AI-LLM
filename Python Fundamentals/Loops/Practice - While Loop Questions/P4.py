f0d15ef7734396682655658c8fc7a07b484add99
zamanuddinkhan <zamanuddinkhan1234@gmail.com>
2026-06-18 17:59:23 +0530

    Refactor implementation to store squares of numbers from 1 to 10 in a list and print the list

--- Python Fundamentals/Loops/Practice - While Loop Questions/P4.py
+++ Python Fundamentals/Loops/Practice - While Loop Questions/P4.py
@@ -1,7 +1,10 @@
-# Que:4 Print the squares from 1 to 10.
+# Que:4 Print the squares from 1 to 10 in List Format.
 
+t = []
 i = 1
-while i <=10:
+while i <= 10:
     n = i * i
-    print(n)
+    t.append(n)
     i = i + 1
+
+print(t)
