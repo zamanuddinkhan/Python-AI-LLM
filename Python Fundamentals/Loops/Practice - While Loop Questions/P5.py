4d639bb6c59804af330ec9d54e0ec2c1ac08b9d0
zamanuddinkhan <zamanuddinkhan1234@gmail.com>
2026-06-18 18:00:26 +0530

    Update question number in the comment for list element printing exercise

--- Python Fundamentals/Loops/Practice - While Loop Questions/P5.py
+++ Python Fundamentals/Loops/Practice - While Loop Questions/P5.py
@@ -1,4 +1,4 @@
-# Que:4 Print the elements of the following list using a loop: [1,4,9,16,25,36,49,64,81,100]
+# Que:5 Print the elements of the following list using a loop: [1,4,9,16,25,36,49,64,81,100]
 # Traverse in Each Element
 
 n = [1,4,9,16,25,36,49,64,81,100]
