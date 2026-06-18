1f00abdc418254b0beac5040d14f691f5eefa598
zamanuddinkhan <zamanuddinkhan1234@gmail.com>
2026-06-18 17:51:39 +0530

    Add implementation to print elements of a predefined list using a while loop

---
+++ Python Fundamentals/Loops/Practice - While Loop Questions/P5.py
@@ -0,0 +1,8 @@
+# Que:4 Print the elements of the following list using a loop: [1,4,9,16,25,36,49,64,81,100]
+# Traverse in Each Element
+
+n = [1,4,9,16,25,36,49,64,81,100]
+i = 0
+while i <= 9:
+    print(n[i])
+    i = i + 1
