6973997c966b329d184b1f69f483c7894de2338e
zamanuddinkhan <zamanuddinkhan1234@gmail.com>
2026-06-21 09:55:02 +0530

    Add implementation of function to print the length of a list

---
+++ Python Fundamentals/Functions & Recursion/P2.py
@@ -0,0 +1,10 @@
+# Que:2 Write a Function to print the length of list. (List is the parameter)
+
+cities = ["noida", "pune", "delhi", "gurgaon", "mumbai", "banglore", "chennai"]
+heroes = ["shaktiman", "thor", "captain america", "batman", "ironman"]
+
+def print_len(ls):
+    print(len(ls))
+
+print_len(cities)
+print_len(heroes)
