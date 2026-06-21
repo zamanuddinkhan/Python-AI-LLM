4cd1565e42a11b9b86b6aece10e4386190641c53
zamanuddinkhan <zamanuddinkhan1234@gmail.com>
2026-06-21 14:52:27 +0530

    Refactor currency converter functions for improved clarity and structure

--- Python Fundamentals/Functions & Recursion/P6.py
+++ Python Fundamentals/Functions & Recursion/P6.py
@@ -1,19 +1,32 @@
-# Que:6.1 WAP convert USD to INR.
+# Que:6 WAP convert USD to INR or INR to USD.
 
-usd = int(input("Enter USD amount: "))
-
-def usd_to_inr(usd):
+def usd_to_inr():
+    usd = int(input("Enter USD amount: "))
     n = usd * 95
     print(usd,"USD =",n,"INR")
 
-usd_to_inr(usd)
-
-# Que:6.1 WAP convert INR to USD.
-
-inr = int(input("Enter INR amount: "))
-
-def inr_to_usd(inr):
+def inr_to_usd():
+    inr = int(input("Enter INR amount: "))
     usd = inr / 95
     print(inr,"INR =",usd,"USD")
 
-inr_to_usd(inr)
+while True:
+    print("\n======== Currency Converter ========")
+    print("1. USD to INR")
+    print("2. INR to USD")
+    print("3. Exit")
+
+    n = int(input("Enter Choice (1-3): "))
+
+    if n == 1:
+        usd_to_inr()
+
+    elif n == 2:
+        inr_to_usd()
+    
+    elif n == 3:
+        print("Exit successfully.")
+        break
+
+    else:
+        print("Invalid choice. Try again.")
