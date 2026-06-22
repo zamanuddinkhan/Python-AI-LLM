beaf93c528eee35b9b83592e80dd1690f8847bde
zamanuddinkhan <zamanuddinkhan1234@gmail.com>
2026-06-23 00:15:43 +0530

    Add factorial function implementation with user input

--- Python Fundamentals/Functions & Recursion/Recurrence Relation.py
+++ Python Fundamentals/Functions & Recursion/Recurrence Relation.py
@@ -13,3 +13,11 @@ Previous terms such as T(n-1) and T(n-2) are used to calculate it.
 A base case is required to stop the recursion.
 """
 
+# Factorial
+def factorial(n):
+    if (n == 0 or n == 1):
+        return 1 
+    return factorial(n-1) * n
+
+n = int(input("Enter Number: "))
+print("Factorial of", n, "is:", factorial(n))
