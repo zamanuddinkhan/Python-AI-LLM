c806288fb86b0b3d1f6a169a964ae576978a2f9f
zamanuddinkhan <zamanuddinkhan1234@gmail.com>
2026-06-23 00:11:15 +0530

    Add documentation for recurrence relations to explain their definition and general form

---
+++ Python Fundamentals/Functions & Recursion/Recurrence Relation.py
@@ -0,0 +1,15 @@
+"""
+A recurrence relation is an equation that defines a sequence using one or more of its previous terms.
+
+In simple words, the current value depends on previous value(s).
+
+General Form
+T(n) = f(T(n-1), T(n-2), ..., n)
+
+where:
+
+T(n) is the current term.
+Previous terms such as T(n-1) and T(n-2) are used to calculate it.
+A base case is required to stop the recursion.
+"""
+
