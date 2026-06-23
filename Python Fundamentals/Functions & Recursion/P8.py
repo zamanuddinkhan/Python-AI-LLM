97cf250c94da4567766dbc051d6e94fae0df044e
zamanuddinkhan <zamanuddinkhan1234@gmail.com>
2026-06-23 08:19:01 +0530

    Implement recursive function to print all elements in a list

--- Python Fundamentals/Functions & Recursion/P8.py
+++ Python Fundamentals/Functions & Recursion/P8.py
@@ -1,3 +1,12 @@
 # Que:8 Write a recursive function to print all elements in a list.
 # Hint: Use list & index as parameters
 
+def print_list(ls, idx = 0):
+    if(idx == len(ls)):
+        return
+    print(ls[idx])
+    print_list(ls, idx + 1)
+
+lst = ["a","b","c","d"]
+print("Elements of List are: ")
+print_list(lst)
