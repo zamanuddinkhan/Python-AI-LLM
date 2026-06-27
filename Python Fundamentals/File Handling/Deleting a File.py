c0d17864e0648e959316f7400daefc40dc3229f6
zamanuddinkhan <zamanuddinkhan1234@gmail.com>
2026-06-28 00:05:09 +0530

    Add Deleting a File.py to demonstrate file deletion using the os module

---
+++ Python Fundamentals/File Handling/Deleting a File.py
@@ -0,0 +1,10 @@
+"""
+Deleting a File: Using the os Module
+
+A module is a Python file (code library) written by other programmers that
+contains functions and classes which we can use in our programs.
+"""
+
+import os
+
+os.remove(r"Python Fundamentals\File Handling\sample.txt")
