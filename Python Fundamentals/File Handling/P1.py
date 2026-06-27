6ab4d4850e4dc2bca77d6b9fe5d45cdfd251d9fc
zamanuddinkhan <zamanuddinkhan1234@gmail.com>
2026-06-28 00:31:11 +0530

    Fix typo in P1.py and ensure file operations are correctly implemented

--- Python Fundamentals/File Handling/P1.py
+++ Python Fundamentals/File Handling/P1.py
@@ -1,10 +1,13 @@
 """
 Create a new file "practice.txt" using python. Add the following data in it:
 Hi everyone
-I am Leanring File I/O
+I am Learning File I/O
 using C
 I like programming in C.
 
 Que:1 WAF that replace all occurrences of "C" with "Python" in above file.
 Search if the word "learning" exists in the file or not.
 """
+f = open("Python Fundamentals\File Handling\practice.txt","a+")
+data = f.write("Hi everyone\nI am Learning File I/O\nusing C\nI like programming in C.")
+f.close()
