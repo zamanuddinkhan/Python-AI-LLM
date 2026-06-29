5b1f8b537781406447996db74e140fd8bdb28c5e
zamanuddinkhan <zamanuddinkhan1234@gmail.com>
2026-06-29 20:02:56 +0530

    Update P1.py to replace programming language references from C to Python and ensure file operations are correctly implemented

--- Python Fundamentals/File Handling/P1.py
+++ Python Fundamentals/File Handling/P1.py
@@ -4,10 +4,8 @@ Hi everyone
 I am Learning File I/O
 using C
 I like programming in C.
-
-Que:1 WAF that replace all occurrences of "C" with "Python" in above file.
-Search if the word "learning" exists in the file or not.
 """
-f = open("Python Fundamentals\File Handling\practice.txt","a+")
-data = f.write("Hi everyone\nI am Learning File I/O\nusing C\nI like programming in C.")
+f = open("Python Fundamentals\File Handling\practice.txt", "w")
+f.write("Hi everyone\nI am Learning File I/O\nusing C\nI like programming in C.")
 f.close()
+
