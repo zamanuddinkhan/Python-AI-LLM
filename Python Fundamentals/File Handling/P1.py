c14a886d6460c579f21908f6be73aea78565540c
zamanuddinkhan <zamanuddinkhan1234@gmail.com>
2026-06-29 20:46:16 +0530

    Refactor P1.py to uncomment file creation and data writing sections; retain functionality for replacing "C" with "Python" and checking for the word "Learning".

--- Python Fundamentals/File Handling/P1.py
+++ Python Fundamentals/File Handling/P1.py
@@ -1,34 +1,34 @@
-# """
-# Create a new file "practice.txt" using python. Add the following data in it:
-# Hi everyone
-# I am Learning File I/O
-# using C
-# I like programming in C.
-# """
-# f = open("Python Fundamentals\File Handling\practice.txt", "w")
-# f.write("Hi everyone\nI am Learning File I/O\nusing C\nI like programming in C.")
-# f.close()
+"""
+Create a new file "practice.txt" using python. Add the following data in it:
+Hi everyone
+I am Learning File I/O
+using C
+I like programming in C.
+"""
+f = open("Python Fundamentals\File Handling\practice.txt", "w")
+f.write("Hi everyone\nI am Learning File I/O\nusing C\nI like programming in C.")
+f.close()
 
-# # Que:1 WAF that replace all occurrences of "C" with "Python" in practice.txt.
-# f = open("Python Fundamentals\File Handling\practice.txt","r")
-# data = f.read()
-# new_data = data.replace("C","Python")
-# print(new_data)
-# f.close()
+# Que:1 WAF that replace all occurrences of "C" with "Python" in practice.txt.
+f = open("Python Fundamentals\File Handling\practice.txt","r")
+data = f.read()
+new_data = data.replace("C","Python")
+print(new_data)
+f.close()
 
-# f = open("Python Fundamentals\File Handling\practice.txt","w")
-# f.write(new_data)
-# f.close()
+f = open("Python Fundamentals\File Handling\practice.txt","w")
+f.write(new_data)
+f.close()
 
-# # Search if the word "learning" exists in the file or not.
-# word = "Learning"
-# f = open("Python Fundamentals\File Handling\practice.txt","r")
-# data = f.read()
-# if(data.find(word) !=-1):
-#     print("Found")
-# else:
-#     print("Not Found")
-# f.close()
+# Search if the word "learning" exists in the file or not.
+word = "Learning"
+f = open("Python Fundamentals\File Handling\practice.txt","r")
+data = f.read()
+if(data.find(word) !=-1):
+    print("Found")
+else:
+    print("Not Found")
+f.close()
 
 # WAF to find in which line of the file does the word "learning" occur first. Print -1 if word not found.
 def check_for_line():
