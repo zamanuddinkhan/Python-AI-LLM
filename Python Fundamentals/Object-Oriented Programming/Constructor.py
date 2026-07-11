75b29d4a8f914bbc947b70ee7462dc91271acb72
zamanuddinkhan <zamanuddinkhan1234@gmail.com>
2026-07-11 15:46:42 +0530

    Update Student constructor to include marks parameter and adjust instantiation examples

--- Python Fundamentals/Object-Oriented Programming/Constructor.py
+++ Python Fundamentals/Object-Oriented Programming/Constructor.py
@@ -7,15 +7,16 @@ In Python, the constructor is named __init__().
 
 class Student:
 
-    def __init__(self, name):
+    def __init__(self, name, marks):
         self.name = name
+        self.marks = marks
         print("Adding new student")
 
-s1 = Student("Karan")
-print(s1.name)
+s1 = Student("Karan",81)
+print(s1.name, s1.marks)
 
-s2 = Student("Arjun")
-print(s2.name)
+s2 = Student("Arjun",72)
+print(s2.name,s1.marks)
 
 """
 Self: Self is a reference to the current object that is using the class. 
