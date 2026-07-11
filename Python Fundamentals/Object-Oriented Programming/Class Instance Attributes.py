daf3ff6faeab2e9f38c0e4a4841f09479d486064
zamanuddinkhan <zamanuddinkhan1234@gmail.com>
2026-07-12 00:28:32 +0530

    Add Class and Instance Attribute explanations with syntax examples

---
+++ Python Fundamentals/Object-Oriented Programming/Class Instance Attributes.py
@@ -0,0 +1,18 @@
+"""
+Class Attribute:
+A class attribute is a variable that belongs to the class and is shared by all objects of that class.
+
+Syntax:
+"""
+class ClassName:
+    class_attribute = value
+
+"""
+Instance Attribute: An instance attribute is a variable that belongs to an object.
+Each object has its own copy of the attribute.
+
+Syntax:
+"""
+class ClassName:
+    def __init__(self, parameter):
+        self.instance_attribute = parameter
