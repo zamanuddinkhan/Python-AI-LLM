8cb807e4dbd1d392a4a67380785f73cfcc676482
zamanuddinkhan <zamanuddinkhan1234@gmail.com>
2026-07-13 00:23:25 +0530

    Add Account class to manage balance with debit and credit methods

---
+++ Python Fundamentals/Object-Oriented Programming/P2.py
@@ -0,0 +1,23 @@
+class Account:
+    def __init__(self, bal, acc):
+        self.bal = bal
+        self.acc_no = acc
+
+    def debit(self, amount):
+        self.bal -= amount
+        print("Rs.",amount,"was debited")
+        print("Total Balance: ",self.get_bal())
+
+    def credit(self, amount):
+        self.bal += amount
+        print("Rs.",amount,"was creadited")
+        print("Total Balance: ",self.get_bal())
+
+    def get_bal(self):
+        return self.bal
+
+acc1 = Account(1020, 12345)
+acc1.debit(1000)
+acc1.credit(500)
+acc1.credit(90500)
+acc1.debit(15000)
